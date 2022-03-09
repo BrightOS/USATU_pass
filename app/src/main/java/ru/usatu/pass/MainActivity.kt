fa8e1ec9571e2624a8aca3fd0def8759799de104
BrightOS <dbarov3@gmail.com>
2022-03-09 07:19:56 +0500

    some changes

--- app/src/main/java/ru/usatu/pass/MainActivity.kt
+++ app/src/main/java/ru/usatu/pass/MainActivity.kt
@@ -1,11 +1,25 @@
 package ru.usatu.pass
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import ru.usatu.pass.databinding.ActivityMainBinding
+import ru.usatu.pass.databinding.FragmentMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: FragmentMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.settings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
